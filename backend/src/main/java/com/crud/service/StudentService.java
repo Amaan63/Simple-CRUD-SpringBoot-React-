ff3746a11b1416eba39d8f28ff78097ff341a007
Amaan63 <amaansayed63@gmail.com>
2025-04-24 20:36:11 +0530

    Made The service method for Update and Delete Student

--- backend/src/main/java/com/crud/service/StudentService.java
+++ backend/src/main/java/com/crud/service/StudentService.java
@@ -6,13 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
 
     @Autowired
     private StudentRepo studentRepo;
-
 
     public Students addStudent(Students student){
         return studentRepo.save(student);
@@ -21,4 +21,19 @@ public class StudentService {
     public List<Students> getAllStudents(){
         return studentRepo.findAll();
     }
+
+    public Students updateStudent(Students updatedStudent) {
+        Optional<Students> existingStudent = studentRepo.findById(updatedStudent.getId());
+        Students studentToUpdate = existingStudent.get();
+        studentToUpdate.setAge(updatedStudent.getAge());
+        studentToUpdate.setName(updatedStudent.getName());
+        studentToUpdate.setDepartment(updatedStudent.getDepartment());
+        return studentRepo.save(studentToUpdate);
+    }
+
+    public boolean deleteStudent(long id){
+        studentRepo.deleteById(id);
+        return true;
+    }
+
 }
