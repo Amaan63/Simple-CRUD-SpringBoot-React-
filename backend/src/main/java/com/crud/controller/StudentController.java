a2742cd8142c8a4ead1002ee4fc02374c425c202
Amaan63 <amaansayed63@gmail.com>
2025-04-24 19:50:45 +0530

    Created Add Student Controller

--- backend/src/main/java/com/crud/controller/StudentController.java
+++ backend/src/main/java/com/crud/controller/StudentController.java
@@ -1,4 +1,14 @@
 package com.crud.controller;
 
+import com.crud.entities.Students;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class StudentController {
+    @PostMapping("/addStudent")
+    public Students addStudentController(@RequestBody Students student){
+        return student;
+    }
 }
