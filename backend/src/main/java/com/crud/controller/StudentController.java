472de6fec978bd2767eea859a0f1083e57d0d5a4
Amaan63 <amaansayed63@gmail.com>
2025-04-24 20:03:30 +0530

    Controller Created for getting All the students

--- backend/src/main/java/com/crud/controller/StudentController.java
+++ backend/src/main/java/com/crud/controller/StudentController.java
@@ -1,14 +1,28 @@
 package com.crud.controller;
 
 import com.crud.entities.Students;
+import com.crud.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
     @PostMapping("/addStudent")
     public Students addStudentController(@RequestBody Students student){
-        return student;
+        return studentService.addStudent(student);
+    }
+
+    @GetMapping("/viewAllStudents")
+    public List<Students> getAllStudents(){
+        return studentService.getAllStudents();
     }
 }
