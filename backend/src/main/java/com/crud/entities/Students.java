d08640834788435dfe244f924d623dfb7e2eec5e
Amaan63 <amaansayed63@gmail.com>
2025-04-24 19:34:23 +0530

    Made the Entity and mapped the table

--- backend/src/main/java/com/crud/entities/Students.java
+++ backend/src/main/java/com/crud/entities/Students.java
@@ -1,4 +1,20 @@
 package com.crud.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "students")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Students {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private int age;
+    private String department;
 }
