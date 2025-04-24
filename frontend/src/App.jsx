75578b1a73a8ebaf84d3c94632c4add241cf7be4
Amaan63 <amaansayed63@gmail.com>
2025-04-24 22:07:26 +0530

    Created Route for Home Component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,12 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import AddStudent from "./component/addStudent";
 import ViewStudent from "./component/viewStudents";
+import Home from "./component/Home";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="add-student" element={<AddStudent />} />
       <Route path="view-students" element={<ViewStudent />} />
     </Routes>
