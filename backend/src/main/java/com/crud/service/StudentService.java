933b97bae81c16a64a1b2a5d07646687e7ce988d
Amaan63 <amaansayed63@gmail.com>
2025-04-24 20:02:27 +0530

    Get All Students Method Created

--- backend/src/main/java/com/crud/service/StudentService.java
+++ backend/src/main/java/com/crud/service/StudentService.java
@@ -1,12 +1,24 @@
 package com.crud.service;
 
 import com.crud.entities.Students;
+import com.crud.repository.StudentRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class StudentService {
-    public Students addStudent(Students student){
 
-        return student;
+    @Autowired
+    private StudentRepo studentRepo;
+
+
+    public Students addStudent(Students student){
+        return studentRepo.save(student);
+    }
+
+    public List<Students> getAllStudents(){
+        return studentRepo.findAll();
     }
 }
