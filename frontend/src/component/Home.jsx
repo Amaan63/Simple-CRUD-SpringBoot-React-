523a23eb0749bd6eb2a5a1fa031f763ec94ef718
Amaan63 <amaansayed63@gmail.com>
2025-04-24 22:11:43 +0530

    Some Styling is Added Using Tailwind

--- frontend/src/component/Home.jsx
+++ frontend/src/component/Home.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Hello this is Home Page</div>;
+  return (
+    <div className="text-center font-semibold text-2xl">
+      Hello this is Home Page
+    </div>
+  );
 };
 
 export default Home;
