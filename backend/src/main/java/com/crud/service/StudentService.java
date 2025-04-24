ac662b83c7150107c9f529e9ee773500ed509fb6
Amaan63 <amaansayed63@gmail.com>
2025-04-24 19:49:44 +0530

    Made the Student Service

---
+++ backend/src/main/java/com/crud/service/StudentService.java
@@ -0,0 +1,12 @@
+package com.crud.service;
+
+import com.crud.entities.Students;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+    public Students addStudent(Students student){
+
+        return student;
+    }
+}
